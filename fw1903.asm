9fec45bd9e3a5700817e5ab531058c8703b77429
mike dan mawira <mawiramikedan2@gmail.com>
2026-07-23 18:40:01 +0300

    single led using30 bytes for 30 leds

--- fw1903.asm
+++ fw1903.asm
@@ -5,6 +5,7 @@
 .def bit_counter = r21
 .def byte_counter =r22
 .def brightness = r19
+.def zero_reg =r4
 .equ ledcntr = PORTD
 .equ ledpin = 4
 .dseg
@@ -18,23 +19,22 @@ clr_led:; subroutine to clear the strip
 	ldi pos,0
 	mov counter,pos
 	clr_loc:
-	st y+,pos
+	st z+,pos
 	inc counter
 	cpi counter,30
 	brne clr_loc
-	sbiw yl,30 ;point to the 1st memory loc allocated
+	sbiw zl,30 ;point to the 1st memory loc allocated
 	pop pos ;recover thee previous pos value
 	ret ;return to caller
-led_write:
-	push counter
-	ldi counter, 3 ;will help us light 20 leds
-	ld_30leds:
-	clc ;clear carry flag
-	ldi byte_counter,3;counts three bytes
-	ldi pos, 10 ; we'l send 30 bytes
+led_write:;does not contain latch so it should be called from the main code space
+	push pos
+	ldi pos,10; ??
+	ld_led:
+	ldi byte_counter,3
 	ld_byte:
-	ldi bit_counter,8 ;load bit counter
-	ld led,x+ ;load the first byte of data
+	clc ;clear the carry flag
+	ldi bit_counter,8
+	ld led,x+ ;load from x pointer
 	check_bit:
 	rol led ;rotate the data
 	brcc bit_is0
@@ -43,23 +43,18 @@ led_write:
 	bit_is0:
 	 rcall bit_0 ;send the sequence for bit 0
 	cont:
-	dec bit_counter;have we sent 8 bits?
-	brne check_bit ;if no then continue checking the bits
+	dec bit_counter ;dec no of bits remaining to be rolled
+	brne check_bit ;if not zero then check bit again
 
-	dec byte_counter ;have we finished loading one byte
-	brne ld_byte ;if yes then load anothe byte
+	dec byte_counter
+	brne ld_byte
 
-	ldi byte_counter,3 ;reload the counters
-	ldi bit_counter, 8 ;reload the counters
-	dec pos ;have we reached the 10th led?
-	brne ld_byte ;load till the 10th led
-	;rcall latch ;show the annimation
-	sbiw xl,30 ;reset the x pointer to point to the innitial mem loc
-	dec counter
-	brne ld_30leds
-	rcall latch
-	pop counter
+	dec pos
+	brne ld_led
+	sbiw yl,30 ;reset it to point to mem adress 1st alocated 0x0060
+	pop pos
 	ret ;return to caller
+
 bit_1: ;subroutine to send a bit 1
 	sbi ledcntr,ledpin
 	ldi r16,4;waste 14 clocks
@@ -123,6 +118,8 @@ latch:
 	pop r22
 	ret ;return to caller
 delay:
+	ldi r16,4
+	start:
 	ldi r23,255
 	outer_1:
 	ldi r24,255
@@ -131,67 +128,127 @@ delay:
 	brne inner_1
 	dec r23
 	brne outer_1
+	dec r16
+	brne start
 	ret ;return to caller
 main:
 	ldi r16, low(ramend)
 	out spl,r16 ;init stack
 
 	sbi DDRD,ledpin
+	ldi zh,high(led_data)
+	ldi zl,low(led_data) ;pointer to clear led_data
 	ldi xh,high(led_data)
 	ldi xl,low(led_data)
 	ldi yh,high(led_data)
-	ldi yl,low(led_data) ;init pointers x and y register
+	ldi yl,low(led_data) ;init pointers z, x and y register
+	ldi r16,0
+	mov zero_reg,r16 ;clear register 4 for use in pointer calcs
+	ldi brightness,100
+loop:
+	ldi pos,1
+check_led:
+	clc ;clear the carry flag
+	cpi pos,11 ;check to determine what bank we will choose
+	brlo bank_1 ;branch to this if pos is less than 10
 
-	ldi brightness,125
-lp_antn:rcall clr_led ;clear the 10 leds
-	rcall delay
-	rcall delay
-ld_again:ldi counter,10
-fw_red:std y+1,brightness ;write to the red led of each chip
-	adiw yl,3 ;progress the counter by 3 illitertions always
-	rcall led_write ;illusion of moving red light left to right
-	rcall delay ;
-	rcall delay
-	rcall delay
+	cpi pos,21
+	brlo bank_2 ;if its less than 20 then it belongs in bank 2
+	brsh is_bank_3
+is_bank_3:
+	rjmp bank_3 ;this is because branch can oly jump+-64 bytes
+
+bank_1:
+	push counter
+	ldi counter,9 ;??
+	mov r3,pos ;this shows us the led that we need to light not the place to point
+	mov r5,r3 ;getting ready to multiply  by 3
+	lsl r3 ;multiply by 2
+	add r3,r5 ;complete the multiplication by 3 this gives you the last color of the number geneated
+	add yl,r3
+	adc yh,zero_reg ;points to the numbers blue light
+	rcall clr_led ;all the mem locs are zerod
+	st y,brightness ;writes only one led
+	rcall led_write;writes the first 30 bytes
+	sub yl,r3
+	sbc yh,zero_reg ;return the pointer to initial pointing pos
 	dec counter
-	brne fw_red ;continue loading red
-	sbiw yl,30 ;points to 1st mem loc
-
-	rcall clr_led ;clear the strip
+	ldi r16,2
+	mov r2,r16 ;move it to r2
+        ld_nullbytes:
+	rcall clr_led ;clear the leds again to send the next 60 btes of empt data
+	rcall led_write ;wite the rest of thr null bytes ;it doesnt need a pointer because clr_led already does all the clearing 
+	dec r2
+	brne ld_nullbytes
+	rcall latch ;display the patern
 	rcall delay
-	rcall delay
-	rcall delay
-
-	adiw yl,31;point to the 31st element
-	ldi counter,10
-rv_red:	sbiw yl,3
+	pop counter
+	rjmp cont_1; jump all the other bank_lable codes
+bank_2:
+	ldi r16,10 ;number which will be subtracted
+	mov r2,r16 ;move it to r2 because r16 will be overwritten
+	mov r3,pos ;so we dont have to modify the thing thus make it crash
+	sub r3,r2 ;the answer will tell us the index of the led that should be on
+	mov r5,r3 ;move the contens to get ready for multiplication
+	lsl r3 ;multiply by 2
+	add r3,r5 ;complete the multiplication by 3
+	add yl,r3
+	adc yh,zero_reg ;point to the blue led of the index led to be lit
+	rcall clr_led ;clear the leds
+	rcall led_write ;write the first blank
 	st y,brightness
-	rcall led_write
-	rcall delay
-	rcall delay
-	rcall delay
-	dec counter
-	brne rv_red
-	sbiw yl,2 ;points 1 less to the allocated memory
-	rcall delay
-	rcall delay
-	rcall delay
-
+	rcall led_write ;this will write it to the 2nd tens
 	rcall clr_led
-	ldi counter 10
-fw_blue:std y+1,brightness
-	adiw yl,3
-	rcall led_write
+	rcall led_write ;write zeros to the 3rd tens of the leds
+	rcall latch ;this displays the light pattern
 	rcall delay
-	rcall delay
-	rcall delay
-	rcall delay
-	rcall delay
-	dec counter
-	brne fw_blue
-	adiw yl,1 ;point to the last +1 alocatedmem loc
+	sub yl,r3
+	sbc yh,zero_reg ;point to the inital position
+	rjmp cont_1 ;jump te bank_3 lable
 
-	ldi counter 10
-rv_blue:sbiw,3
+bank_3:
+	ldi r16,20 ;numbe well suntract from pos to get led index
+	mov r2,r16 ;copy it so it wount be overwritten
+	sub r3,r2 ;create the index
+	mov r3,pos ;copy to another register so we dont overide it
+	mov r5,r3 ;copy the number to the register for multiplication purposes
+	lsl r3 ;r3*2
+	add r3,r5 ;complete multiplication by 3
+	add yl,r3
+	adc yh,zero_Reg ;point to the blue light of the indexed led
+	ldi counter, 2
+	ld_nullbyte_3:
+	rcall clr_led
+	rcall led_write
+	dec counter
+	brne ld_nullbyte_3 ;this loads the 1,2 tens with zero
 	st y,brightness
-	
+	rcall led_write
+	rcall latch ;show the animation
+	rcall delay
+	sub yl,r3
+	sbc yh,zero_reg ;point to the initial mem loc it was in
+cont_1:
+	inc pos
+	cpi pos,30 ;have we reached 30 leds?
+	brne is_led_check ;??? branch will fail due to range
+	rjmp cont_2 ;if it fails it jumps to cont_2
+is_led_check:
+	rjmp check_led ;this is due to branch +- 64 bytes jump constrains
+cont_2:
+	rjmp loop ;loops for ever 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
